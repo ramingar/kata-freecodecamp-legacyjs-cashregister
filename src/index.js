e57cfb02da026b4c10d5862bf85683c2fa82dcd9
Rafael Minguet <rafael.minguet@martico.com>
2024-05-14 16:10:42 +0200

    Project's initial structure

---
+++ src/index.js
@@ -0,0 +1,4 @@
+function checkCashRegister() {
+}
+
+export {checkCashRegister}
---
+++ tests/cashRegister.test.js
@@ -0,0 +1,53 @@
+import test from 'tape';
+import {checkCashRegister} from '../src';
+
+// TESTS
+test('-------- Test 1', (assert) => {
+    const expected = {status: "OPEN", change: [["QUARTER", 0.5]]};
+    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+
+    const actual = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+    assert.deepEquals(actual, expected, message);
+    assert.end();
+});
+
+test('-------- Test 2', (assert) => {
+    const expected = {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]};
+    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+
+    const actual = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+    assert.deepEquals(actual, expected, message);
+    assert.end();
+});
+
+test('-------- Test 3', (assert) => {
+    const expected = {status: "INSUFFICIENT_FUNDS", change: []};
+    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+
+    const actual = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+
+    assert.deepEquals(actual, expected, message);
+    assert.end();
+});
+
+test('-------- Test 4', (assert) => {
+    const expected = {status: "INSUFFICIENT_FUNDS", change: []};
+    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+
+    const actual = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+
+    assert.deepEquals(actual, expected, message);
+    assert.end();
+});
+
+test('-------- Test 5', (assert) => {
+    const expected = {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]};
+    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+
+    const actual = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+
+    assert.deepEquals(actual, expected, message);
+    assert.end();
+});
