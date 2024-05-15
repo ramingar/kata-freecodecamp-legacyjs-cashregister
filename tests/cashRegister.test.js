723d0cc0c8ca7735581f3378ceba661b55fdd553
Rafael Minguet <rafael.minguet@martico.com>
2024-05-15 09:22:48 +0200

    Test file

--- tests/cashRegister.test.js
+++ tests/cashRegister.test.js
@@ -4,7 +4,7 @@ import {checkCashRegister} from '../src';
 // TESTS
 test('-------- Test 1', (assert) => {
     const expected = {status: "OPEN", change: [["QUARTER", 0.5]]};
-    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+    const message  = `After checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return ${JSON.stringify(expected)}.`;
 
     const actual = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
@@ -14,7 +14,7 @@ test('-------- Test 1', (assert) => {
 
 test('-------- Test 2', (assert) => {
     const expected = {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]};
-    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+    const message  = `After checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return ${JSON.stringify(expected)}.`;
 
     const actual = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
@@ -24,7 +24,7 @@ test('-------- Test 2', (assert) => {
 
 test('-------- Test 3', (assert) => {
     const expected = {status: "INSUFFICIENT_FUNDS", change: []};
-    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+    const message  = `After checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return ${JSON.stringify(expected)}.`;
 
     const actual = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 
@@ -34,7 +34,7 @@ test('-------- Test 3', (assert) => {
 
 test('-------- Test 4', (assert) => {
     const expected = {status: "INSUFFICIENT_FUNDS", change: []};
-    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+    const message  = `After checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return ${JSON.stringify(expected)}.`;
 
     const actual = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 
@@ -44,7 +44,7 @@ test('-------- Test 4', (assert) => {
 
 test('-------- Test 5', (assert) => {
     const expected = {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]};
-    const message  = `After checkCashRegister(...) should return ${JSON.stringify(expected)}.`;
+    const message  = `After checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return ${JSON.stringify(expected)}.`;
 
     const actual = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 
